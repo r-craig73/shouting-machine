d864959798936f5a3bf09b5056029539f31d5c67
Ron Craig <ron.craig@comcast.net>
2018-09-29 16:21:50 -0700

    remove whitespace and insert indentation

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,8 @@ $(document).ready(function() {
   $("#shoutBack").submit(function(event) {
   var shoutTextInput = $("input#shoutText").val();
   var shoutCaps = shoutTextInput.toUpperCase();
-
   $(".shoutText").text(shoutTextInput);
   $(".shoutCaps").text(shoutCaps);
-
-
-//str.toUpperCase();
-event.preventDefault();
-
-});
+  event.preventDefault();
+  });
 });
